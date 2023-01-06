bd46a155ecb307c991ae82b209d73e7877eea768
KyleHuang <zs.matrix62@gmail.com>
2023-01-06 15:29:49 +0800

    +

--- lua/plugins/editing/outline.lua
+++ lua/plugins/editing/outline.lua
@@ -1,8 +1,24 @@
 return {
+	-- require = function(use)
+	-- 	use 'simrat39/symbols-outline.nvim'
+	-- 	require("symbols-outline").setup()
+	--
+	-- 	vim.api.nvim_set_keymap("", "<space>t", ":SymbolsOutline<cr>", { silent = true })
+	-- end
 	require = function(use)
-		use 'simrat39/symbols-outline.nvim'
-		require("symbols-outline").setup()
-
-		vim.api.nvim_set_keymap("", "<space>t", ":SymbolsOutline<cr>", { silent = true })
-	end
+		use({
+			"stevearc/aerial.nvim",
+			config = function()
+				require("aerial").setup({
+					layout = {
+						min_width = 30,
+					},
+					close_automatic_events = {
+						-- "switch_buffer",
+					},
+				})
+				vim.api.nvim_set_keymap("", "<space>t", ":AerialToggle right<cr>", { silent = true })
+			end,
+		})
+	end,
 }
