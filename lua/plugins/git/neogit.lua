b855e9ed3aa6469004bf528a3f5bd96c3287dc60
KyleHuang <zs.matrix62@gmail.com>
2023-02-21 16:13:25 +0800

    +

--- lua/plugins/git/neogit.lua
+++ lua/plugins/git/neogit.lua
@@ -5,6 +5,8 @@ return {
 			config = function()
 				local neogit = require("neogit")
 				neogit.setup({})
+
+				vim.api.nvim_set_keymap("n", "<leader>gi", ":lua require('neogit').open()<cr>", {})
 			end,
 		})
 	end,
