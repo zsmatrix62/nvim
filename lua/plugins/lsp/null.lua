3ddc1784018c25dba708719591089abd69569b9a
KyleHuang <zs.matrix62@gmail.com>
2023-02-23 18:53:50 +0800

    Add support for golangci_lint_ls, cssmodules_ls, stylelint_lsp, docker_compose_language_service, eslint and remove eslint_d and golangci_lint from null.lua

--- lua/plugins/lsp/null.lua
+++ lua/plugins/lsp/null.lua
@@ -23,8 +23,8 @@ return {
 						null_ls.builtins.formatting.swiftformat,
 						-- lints
 						null_ls.builtins.diagnostics.pylint,
-						null_ls.builtins.diagnostics.eslint_d,
-						null_ls.builtins.diagnostics.golangci_lint,
+						-- null_ls.builtins.diagnostics.eslint_d,
+						-- null_ls.builtins.diagnostics.golangci_lint,
 					},
 					on_attach = function(client, bufnr)
 						if client.supports_method("textDocument/formatting") then
